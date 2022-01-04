7e80b7ef6d2d9318e82530b3cc6563281cbc93f4
tulasacra <tulasacra@gmail.com>
2022-01-04 17:40:51 +0100

    list command now displays tgId for users with no username

--- src/lib/bot.js
+++ src/lib/bot.js
@@ -528,7 +528,7 @@ Available commands:
       for (let i = 0; i < users.length; i++) {
         const thisUser = users[i]
 
-        outStr += `@${thisUser.username ?? thisUser.tgId}\n`
+        if (thisUser.username) { outStr += `@${thisUser.username}\n` } else { outStr += `tgId: ${thisUser.tgId}\n` }
       }
       outStr += '\n'
 
