5a43f3f7155ff9d07ac10537f3bdec2146794ddd
scinklja <73796084+scinklja@users.noreply.github.com>
2022-07-25 00:16:32 +0200

    switch to "web3" infra (#2)
    * changed reference to merit library
    
    * eslint version corrected
    
    * eslint versions corrected
    
    * reference to merit lib changed
    
    * reference to merit lib changed
    
    * npm update
    
    * npm update
    
    * library refernce updated
    
    * env added and update
    
    * switch to "web3" infra
    
    * npm update
    
    * npm update
    
    * npm update
    
    * npm update

--- src/lib/bch.js
+++ src/lib/bch.js
@@ -4,13 +4,19 @@
 
 // Public npm libraries
 const BCHJS = require('@psf/bch-js')
-const BchMessage = require('bch-message-lib')
+const BchMerit = require('bch-merit-lib')
+const BchWallet = require('minimal-slp-wallet/index')
 
 class Bch {
   constructor () {
     // Encapsulate dependencies
     this.bchjs = new BCHJS()
-    this.bchMsg = new BchMessage({ bchjs: this.bchjs }, process.env)
+    this.wallet = new BchWallet(undefined, {
+      noUpdate: true,
+      interface: 'consumer-api',
+      restURL: 'https://free-bch.fullstack.cash'
+    })
+    this.bchMerit = new BchMerit({ wallet: this.wallet, env: process.env })
   }
 
   // Verify that the signed message 'verify' was signed by a specific BCH address.
@@ -40,7 +46,7 @@ class Bch {
     try {
       // Get the aggregated merit of the address.
       if (process.env.VERBOSE_LOG >= 1) console.log(`getMerit slpAddr: ${slpAddr} tokenId: ${process.env.TOKEN_ID}`)
-      const merit = await this.bchMsg.merit.agMerit(slpAddr, process.env.TOKEN_ID)
+      const merit = await this.bchMerit.merit.agMerit(slpAddr, process.env.TOKEN_ID)
       console.log(`merit: ${merit}`)
 
       return merit
